79876fcff2d95445b6c3dcabf4062805e17bf137
Igor Santos <igor.santos@ntconsult.com.br>
2017-06-27 10:32:45 -0300

    Update OSX install file

--- install/macosx.sh
+++ install/macosx.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# TODO
+sudo xcode-select --install
+
 sudo easy_install pip
 
 sudo CFLAGS=-Qunused-arguments CPPFLAGS=-Qunused-arguments pip install ansible
 
 sudo pip install pip --upgrade
 sudo pip install setuptools --upgrade
-sudo pip install ansible
+sudo pip install ansible --quiet
+sudo pip install ansible --upgrade
 
 cd ../provisioning/
 
 ansible-playbook site.yml
+
+
